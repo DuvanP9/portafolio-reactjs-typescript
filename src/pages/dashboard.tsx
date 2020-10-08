6f1c389fb3355ca180e5e709cc94260eb19cfe66
DuvanP9 <dpedroza@dbsolution.com.co>
2020-10-07 21:07:39 -0500

    add-components-material

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import TemporaryDrawer from '../components/DrawerDashboard';
+
+
+function dashboard() {
+
+  return (
+      <div>
+          <TemporaryDrawer/>
+
+      </div>
+  );
+}
+
+export default dashboard;
