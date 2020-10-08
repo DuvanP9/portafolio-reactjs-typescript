be162d964507d1c53f4f8f1810446fd188ce93cd
DuvanP9 <dpedroza@dbsolution.com.co>
2020-10-08 08:22:59 -0500

    act-Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Button from '@material-ui/core/Button';
+
+import './styles/Home.css';
+
 
 function Home() {
 
   return (
+    <div className="container">
       <div>
-          <h1>Home Works</h1>
+        <Link className="Navbar__brand" to="/Landing">
+      <Button variant="contained">Landing</Button>
+      </Link>
       </div>
+      <div>
+        <Link className="Navbar__brand" to="/Dashboard">
+      <Button variant="contained">Dashboard</Button>
+      </Link>
+      </div>
+    </div>
   );
 }
 
